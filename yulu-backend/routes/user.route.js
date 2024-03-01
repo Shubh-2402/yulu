f6fd1d8b2c4d992a7cb04704146c21392409dd86
Shubh-2402 <patilshubham852@gmail.com>
2024-03-01 12:12:30 +0530

    feat: authentication and authorization middlewares

--- yulu-backend/routes/user.route.js
+++ yulu-backend/routes/user.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { userController } = require("../controllers");
+const { authenticateToken } = require("../middlewares/authenticateToken");
+const { authorizeRoles } = require("../middlewares/authorizeRoles");
 const router = express.Router();
 
 router
-  .get("/", userController.getUsers)
-  .get("/:id", userController.getUser)
-  .patch("/:id", userController.updateUser)
-  .delete("/:id", userController.deleteUser)
+  .get("/", authenticateToken, authorizeRoles("user", "admin"), userController.getUsers)
+  .get("/:id", authenticateToken, authorizeRoles("user", "admin"), userController.getUser)
+  .patch("/:id", authenticateToken, authorizeRoles("admin"), userController.updateUser)
+  .delete("/:id", authenticateToken, authorizeRoles("admin"), userController.deleteUser)
 
 module.exports = router;
